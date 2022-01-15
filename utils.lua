a263cd162d0261475599e58db7a9a49639d9d356
Matteo De Giorgi <matteo.dgiorgi@gmail.com>
2022-01-15 23:48:19 +0100

    Added function for filling whitespaces in strings either at the end of beginning. Used fill and fill_end function to include PIDs in Processes section and better format indicators in the format current / max.

---
+++ utils.lua
@@ -0,0 +1,7 @@
+function conky_fill( str, length )
+  return string.format('%' .. conky_parse(length) .. 's', conky_parse(str))
+end
+
+function conky_fill_end( str, length )
+  return string.format('%-' .. conky_parse(length) .. 's', conky_parse(str))
+end
